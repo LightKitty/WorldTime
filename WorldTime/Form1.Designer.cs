7b46c667bdcdddff640f2860c6e5f681f7311747
wnmwi <383929371@qq.com>
2020-11-22 14:27:47 +0800

    first

--- WorldTime/Form1.Designer.cs
+++ WorldTime/Form1.Designer.cs
@@ -28,13 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dateTimePickerSource = new System.Windows.Forms.DateTimePicker();
+            this.comboBoxTimeZoneSource = new System.Windows.Forms.ComboBox();
+            this.comboBoxTimeZoneTarget = new System.Windows.Forms.ComboBox();
+            this.textBoxTarget = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // dateTimePickerSource
+            // 
+            this.dateTimePickerSource.Location = new System.Drawing.Point(12, 12);
+            this.dateTimePickerSource.Name = "dateTimePickerSource";
+            this.dateTimePickerSource.Size = new System.Drawing.Size(160, 23);
+            this.dateTimePickerSource.TabIndex = 0;
+            // 
+            // comboBoxTimeZoneSource
+            // 
+            this.comboBoxTimeZoneSource.FormattingEnabled = true;
+            this.comboBoxTimeZoneSource.Location = new System.Drawing.Point(178, 12);
+            this.comboBoxTimeZoneSource.Name = "comboBoxTimeZoneSource";
+            this.comboBoxTimeZoneSource.Size = new System.Drawing.Size(390, 25);
+            this.comboBoxTimeZoneSource.TabIndex = 2;
+            // 
+            // comboBoxTimeZoneTarget
+            // 
+            this.comboBoxTimeZoneTarget.FormattingEnabled = true;
+            this.comboBoxTimeZoneTarget.Location = new System.Drawing.Point(178, 41);
+            this.comboBoxTimeZoneTarget.Name = "comboBoxTimeZoneTarget";
+            this.comboBoxTimeZoneTarget.Size = new System.Drawing.Size(390, 25);
+            this.comboBoxTimeZoneTarget.TabIndex = 3;
+            // 
+            // textBoxTarget
+            // 
+            this.textBoxTarget.BackColor = System.Drawing.SystemColors.Window;
+            this.textBoxTarget.Location = new System.Drawing.Point(12, 43);
+            this.textBoxTarget.Name = "textBoxTarget";
+            this.textBoxTarget.ReadOnly = true;
+            this.textBoxTarget.Size = new System.Drawing.Size(160, 23);
+            this.textBoxTarget.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(584, 81);
+            this.Controls.Add(this.textBoxTarget);
+            this.Controls.Add(this.comboBoxTimeZoneTarget);
+            this.Controls.Add(this.comboBoxTimeZoneSource);
+            this.Controls.Add(this.dateTimePickerSource);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WorldTime";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePickerSource;
+        private System.Windows.Forms.ComboBox comboBoxTimeZoneSource;
+        private System.Windows.Forms.ComboBox comboBoxTimeZoneTarget;
+        private System.Windows.Forms.TextBox textBoxTarget;
     }
 }
 
--- WorldTime/Form1.cs
+++ WorldTime/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
@@ -12,9 +13,57 @@ namespace WorldTime
 {
     public partial class Form1 : Form
     {
+        ReadOnlyCollection<TimeZoneInfo> timeZoneInfos = TimeZoneInfo.GetSystemTimeZones();
+        string[] timeZoneInfoTexts = null;
+
         public Form1()
         {
             InitializeComponent();
+
+            dateTimePickerSource.CustomFormat = "yyyy-MM-dd HH:mm:ss";
+            dateTimePickerSource.Format = DateTimePickerFormat.Custom;
+            //dateTimePickerTarget.CustomFormat = "yyyy-MM-dd HH:mm:ss";
+            //dateTimePickerTarget.Format = DateTimePickerFormat.Custom;
+
+            timeZoneInfoTexts = new string[timeZoneInfos.Count()];
+            for (int i = 0; i < timeZoneInfos.Count(); i++)
+            {
+                timeZoneInfoTexts[i] = timeZoneInfos[i].DisplayName;
+            }
+            comboBoxTimeZoneSource.Items.AddRange(timeZoneInfoTexts);
+            comboBoxTimeZoneTarget.Items.AddRange(timeZoneInfoTexts);
+
+            comboBoxTimeZoneSource.SelectedIndex = timeZoneInfos.IndexOf(TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time"));
+            comboBoxTimeZoneTarget.SelectedIndex = timeZoneInfos.IndexOf(TimeZoneInfo.FindSystemTimeZoneById("China Standard Time"));
+
+            DateTime utcDtNow = DateTime.UtcNow;
+            dateTimePickerSource.Value = TimeZoneInfo.ConvertTimeFromUtc(utcDtNow, timeZoneInfos[comboBoxTimeZoneSource.SelectedIndex]);
+
+            RefreshTimeText();
+
+            this.dateTimePickerSource.ValueChanged += new System.EventHandler(this.dateTimePickerSource_ValueChanged);
+            this.comboBoxTimeZoneSource.SelectedIndexChanged += new System.EventHandler(this.comboBoxTimeZoneSource_SelectedIndexChanged);
+            this.comboBoxTimeZoneTarget.SelectedIndexChanged += new System.EventHandler(this.comboBoxTimeZoneTarget_SelectedIndexChanged);
+        }
+
+        private void dateTimePickerSource_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshTimeText();
+        }
+
+        private void comboBoxTimeZoneSource_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshTimeText();
+        }
+
+        private void comboBoxTimeZoneTarget_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshTimeText();
+        }
+
+        void RefreshTimeText()
+        {
+            textBoxTarget.Text = TimeZoneInfo.ConvertTime(dateTimePickerSource.Value, timeZoneInfos[comboBoxTimeZoneSource.SelectedIndex], timeZoneInfos[comboBoxTimeZoneTarget.SelectedIndex]).ToString("yyyy-MM-dd HH:mm:ss");
         }
     }
 }
